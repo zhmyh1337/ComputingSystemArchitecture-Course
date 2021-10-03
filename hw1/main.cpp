1234f3e7fc147b4225028c3da5658786cc23a9e3
zhmyh1337 <70749157+zhmyh1337@users.noreply.github.com>
2021-10-03 14:15:17 +0300

    Add project files.

---
+++ hw1/main.cpp
@@ -0,0 +1,12 @@
+﻿// СomputingSystemArchitecture-Course.cpp : Defines the entry point for the application.
+//
+
+#include "СomputingSystemArchitecture-Course.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
