c66676f7f97acd217f3ecf7df3104695f7ba5900
zhmyh1337 <70749157+zhmyh1337@users.noreply.github.com>
2021-10-03 14:16:33 +0300

    Build fix

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,12 +1,7 @@
-﻿// СomputingSystemArchitecture-Course.cpp : Defines the entry point for the application.
-//
-
-#include "СomputingSystemArchitecture-Course.h"
-
-using namespace std;
+﻿#include <iostream>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << "Hello CMake." << std::endl;
 	return 0;
 }
